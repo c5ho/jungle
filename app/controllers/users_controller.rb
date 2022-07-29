607f7d6d1ef57ea8ab0dc0449c4eafdf01ab7d0e
Calvin Ho <calvin.ch.ho@gmail.com>
2022-07-29 02:16:39 -0400

    Fix sign-up with firstname, lastname

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,32 @@
 class UsersController < ApplicationController
-  # ----- add these lines here: -----
-  
-    def new
-      @user = User.new
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    # store all emails in lowercase to avoid duplicates and case-sensitive login errors:
+    @user.email.downcase!
+    
+    if @user.save
+      # If user saves in the db successfully:
+      flash[:notice] = "Account created successfully!"
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      # If user fails model validation - probably a bad password or duplicate email:
+      flash.now.alert = "Oops, couldn't create account. Please make sure you are using a valid email and password and try again."
+      render :new
     end
-  
-    def create
-      @user = User.new(user_params)
-      
-      # store all emails in lowercase to avoid duplicates and case-sensitive login errors:
-      @user.email.downcase!
-      
-      if @user.save
-        # If user saves in the db successfully:
-        flash[:notice] = "Account created successfully!"
-        redirect_to root_path
-      else
-        # If user fails model validation - probably a bad password or duplicate email:
-        flash.now.alert = "Oops, couldn't create account. Please make sure you are using a valid email and password and try again."
-        render :new
-      end
-    end
-  
+  end
+
   private
-  
     def user_params
       # strong parameters - whitelist of allowed fields #=> permit(:name, :email, ...)
       # that can be submitted by a form to the user model #=> require(:user)
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
     end
-    
-  # ----- end of added lines -----
-  end
+
+end
