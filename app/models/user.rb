156e5fc90276382f6e018933891bd694745cfddc
Calvin Ho <calvin.ch.ho@gmail.com>
2022-07-28 23:07:47 -0400

    Modify new user form to work with firstname and lastname

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_secure_password
+	has_secure_password
 
 	# Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
 	validates :email, presence: true, uniqueness: true
