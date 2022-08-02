fc7a638a744e37be0e363571da3475bb769a00a2
Calvin Ho <calvin.ch.ho@gmail.com>
2022-08-02 16:43:35 -0400

    Fix authenticate_with_credentials method and add before(:each) to user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
 
 	# Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
 	validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, format: { without: /\s/ }
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :password, length: { minimum: 5 }  
 
   def self.authenticate_with_credentials(email, password)
-    user = where('LOWER(email) = ?', email.downcase.delete(' '))[0]
+    user = User.find_by(email: email.downcase.strip)
     user && user.authenticate(password)
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,59 +2,55 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  before(:each) do
+    @new_user = User.new({
+      firstname: 'Calvin',
+      lastname: 'Hobbes',
+      email: 'blah@blah.com',
+      password: '12345',
+      password_confirmation: '12345'
+    })
+    @new_user.save
+  end
+  
   # These validations are for new user creation
   describe 'validations' do
+    
     it 'should be successfull when all fields are valid' do
-      new_user = User.new({
-        firstname: 'Calvin',
-        lastname: 'Hobbes',
-        email: 'blah@blah.com',
-        password: '12345',
-        password_confirmation: '12345'
-      })
-      expect(new_user).to be_valid
+      expect(@new_user).to be_valid
     end
 
     it 'should not be successfull when missing firstname' do
-      new_user = User.new
-      expect(new_user).not_to be_valid
-      expect(new_user.errors.messages[:firstname]).to include('can\'t be blank')
+      @new_user.firstname = nil
+      expect(@new_user).not_to be_valid
+      expect(@new_user.errors.messages[:firstname]).to include('can\'t be blank')
     end
 
     it 'should not be successfull when missing lastname' do
-      new_user = User.new
-      expect(new_user).not_to be_valid
-      expect(new_user.errors.messages[:lastname]).to include('can\'t be blank')
+      @new_user.lastname = nil
+      expect(@new_user).not_to be_valid
+      expect(@new_user.errors.messages[:lastname]).to include('can\'t be blank')
     end
 
     it 'should not be successfull when missing email' do
-      new_user = User.new
-      expect(new_user).not_to be_valid
-      expect(new_user.errors.messages[:email]).to include('can\'t be blank')
+      @new_user.email = nil
+      expect(@new_user).not_to be_valid
+      expect(@new_user.errors.messages[:email]).to include('can\'t be blank')
+    end
+
+    it 'should not be successfull when white spaces in email' do
+      @new_user.email = '         blah@blah.com   '
+      expect(@new_user).not_to be_valid
+      expect(@new_user.errors.messages[:email]).to include('is invalid')
     end
     
     it 'should not be successfull when password confirmation is blank' do
-      new_user = User.new({
-        firstname: 'Calvin',
-        lastname: 'Hobbes',
-        email: 'blah@blah.com',
-        password: '12345',
-        password_confirmation: ''
-      })
-      expect(new_user).not_to be_valid
-      expect(new_user.errors.messages[:password_confirmation]).to include('doesn\'t match Password')    
+      @new_user.password_confirmation = ''
+      expect(@new_user).not_to be_valid
+      expect(@new_user.errors.messages[:password_confirmation]).to include('doesn\'t match Password')    
     end
 
     it 'should not be successfull when email is not unique' do
-      new_user = User.new({
-        firstname: 'Calvin',
-        lastname: 'Hobbes',
-        email: 'blah@blah.com',
-        password: '12345',
-        password_confirmation: '12345'
-      })
-      new_user.save
-
       another_new_user = User.new({
         firstname: 'John',
         lastname: 'Doe',
@@ -64,90 +60,45 @@ RSpec.describe User, type: :model do
       })
       another_new_user.save
 
-      expect(new_user).to be_valid
+      expect(@new_user).to be_valid
       expect(another_new_user).not_to be_valid
       expect(another_new_user.errors.messages[:email]).to include('has already been taken')
     end
 
     it 'should not be successfull when password length is less than 5 characters' do
-      new_user = User.new({
-        firstname: 'Calvin',
-        lastname: 'Hobbes',
-        email: 'blah@blah.com',
-        password: '1234',
-        password_confirmation: '1234'
-      })
-      expect(new_user).not_to be_valid
+      @new_user.password = '1234'
+      expect(@new_user).not_to be_valid
       # use match to match a substring within a string
-      expect(new_user.errors.messages[:password]).to include(match(/is too short/))
+      expect(@new_user.errors.messages[:password]).to include(match(/is too short/))
     end
   end
 
   # These validations are for user login
   describe 'authentications' do
+
     it 'should be successfull if password and email are valid' do
-      new_user = User.new({
-        firstname: 'Calvin',
-        lastname: 'Hobbes',
-        email: 'blah@blah.com',
-        password: '12345',
-        password_confirmation: '12345'
-      })
-      new_user.save
       valid_user = User.authenticate_with_credentials('blah@blah.com', '12345')
-      expect(valid_user).to eq(new_user)
+      expect(valid_user).to eq(@new_user)
     end
   
     it 'should not be successful if password is wrong' do
-      new_user = User.new({
-        firstname: 'Calvin',
-        lastname: 'Hobbes',
-        email: 'blah@blah.com',
-        password: '12345',
-        password_confirmation: '12345'
-      })
-      new_user.save
       invalid_user = User.authenticate_with_credentials('blah@blah.com', '123456')
-      expect(invalid_user).to_not eq(new_user)
+      expect(invalid_user).to_not eq(@new_user)
     end
 
     it 'should not be successful if email is wrong' do
-      new_user = User.new({
-        firstname: 'Calvin',
-        lastname: 'Hobbes',
-        email: 'blah@blah.com',
-        password: '12345',
-        password_confirmation: '12345'
-      })
-      new_user.save
       invalid_user = User.authenticate_with_credentials('bla@blah.com', '12345')
-      expect(invalid_user).to_not eq(new_user)
+      expect(invalid_user).to_not eq(@new_user)
     end
   
     it 'should be successful if user adds uppercase letters to their email' do
-      new_user = User.new({
-        firstname: 'Calvin',
-        lastname: 'Hobbes',
-        email: 'blah@blah.com',
-        password: '12345',
-        password_confirmation: '12345'
-      })
-      new_user.save
       valid_user = User.authenticate_with_credentials('Blah@BLAH.com', '12345')
-      expect(valid_user).to eq(new_user)
+      expect(valid_user).to eq(@new_user)
     end
 
     it 'should authenticate if user adds spaces to beginning or end of email' do
-      new_user = User.new({
-        firstname: 'Calvin',
-        lastname: 'Hobbes',
-        email: 'blah@blah.com',
-        password: '12345',
-        password_confirmation: '12345'
-      })
-      new_user.save
       valid_user = User.authenticate_with_credentials('      blah@blah.com ', '12345')
-      expect(valid_user).to eq(new_user)
+      expect(valid_user).to eq(@new_user)
     end
   end
 end
