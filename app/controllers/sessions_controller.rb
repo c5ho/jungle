868332ac5b73c0d1ef100e5f3106501e2ceccc1a
Calvin Ho <calvin.ch.ho@gmail.com>
2022-08-02 12:30:44 -0400

    Update sessions controller to use authenticate_with_credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     
     # Verify user exists in db and run has_secure_password's .authenticate() 
     # method to see if the password submitted on the login form was correct: 
-    if user && user.authenticate(params[:login][:password]) 
+    if user && user.authenticate_with_credentials(params[:login][:password]) 
       # Save the user.id in that user's session cookie:
       session[:user_id] = user.id.to_s
       redirect_to root_path, notice: 'Successfully logged in!'
